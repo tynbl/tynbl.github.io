adfbba60553c54e776d8fe2fb0cd9c9cf1a4e431
tynbl <tynbl@sohu.com>
2017-05-31 13:29:38 +0800

    jekyll
    jekyll demo

--- apps/blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from .views import *
-
-urlpatterns = [
-    url(r'^comment/$', CommentsView.as_view(), name='comment'),
-    url(r'^reply/$', ReplyView.as_view(), name='reply'),
-]
